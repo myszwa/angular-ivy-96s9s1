ca0dd5ed3f9eb37a61bdc80a7deca32f22db275a
myszwa <patryk.janikowski@gmail.com>
2022-08-11 12:16:56 +0200

    3 files modified, 4 added

---
+++ src/app/decors/decors.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decors',
+  templateUrl: './decors.component.html',
+  styleUrls: ['./decors.component.scss'],
+})
+export class DecorsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
