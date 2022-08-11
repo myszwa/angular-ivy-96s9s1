ae55edf69a5e55ecd7b070807e40bd887519a6b5
myszwa <patryk.janikowski@gmail.com>
2022-08-11 11:48:42 +0200

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   imports: [BrowserModule, FormsModule, MatTabsModule, BrowserAnimationsModule],
   declarations: [AppComponent, HelloComponent, TabsComponent],
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css'],
+  styleUrls: ['./tabs.component.scss'],
 })
 export class TabsComponent implements OnInit {
   constructor() {}
