0c5367e0c53e5029561155f14333e5d6011038be
myszwa <patryk.janikowski@gmail.com>
2022-08-12 12:26:28 +0200

    7 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
@@ -9,9 +9,36 @@ import { TabsComponent } from './tabs/tabs.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DecorsComponent } from './decors/decors.component';
 
+import {
+  DialogOverviewExample,
+  DialogOverviewExampleDialog,
+} from './popup/dialog-overview-example';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatTabsModule, BrowserAnimationsModule],
-  declarations: [AppComponent, HelloComponent, TabsComponent, DecorsComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatInputModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TabsComponent,
+    DecorsComponent,
+    DialogOverviewExample,
+    DialogOverviewExampleDialog,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
