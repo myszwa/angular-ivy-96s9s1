cbb79e70cb69d03ed31cd5dd2b4df91ea2e815db
myszwa <patryk.janikowski@gmail.com>
2022-08-12 14:38:34 +0200

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   imports: [
     BrowserModule,
@@ -29,7 +30,7 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatTabsModule,
     MatDialogModule,
-    MatInputModule
+    MatInputModule,
   ],
   declarations: [
     AppComponent,
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -13,4 +13,20 @@ export class TabsComponent implements OnInit {
   getValues(val) {
     console.warn(val);
   }
+
+  selectedIndex: number = 0;
+  maxNumberOfTabs: number = 2;
+  nextStep() {
+    if (this.selectedIndex != this.maxNumberOfTabs && this.selectedIndex <= 1) {
+      this.selectedIndex = this.selectedIndex + 1;
+    }
+    console.log(this.selectedIndex);
+  }
+
+  previousStep() {
+    if (this.selectedIndex != 0) {
+      this.selectedIndex = this.selectedIndex - 1;
+    }
+    console.log(this.selectedIndex);
+  }
 }
