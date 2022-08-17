302582f47f07030e9a996dc80f8f7d148b8d61cc
myszwa <patryk.janikowski@gmail.com>
2022-08-17 13:46:17 +0200

    6 files modified, 1 added, 1 deleted

--- src/app/upload-image/upload-image.component.ts
+++ src/app/upload-image/upload-image.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload-image',
   templateUrl: './upload-image.component.html',
-  styleUrls: ['./upload-image.component.css']
+  styleUrls: ['./upload-image.component.scss'],
 })
 export class UploadImageComponent implements OnInit {
+  imageUrl: string = '';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onselectFile(e) {
+    if (e.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = (event: any) => {
+        this.imageUrl = event.target.result;
+      };
+    }
   }
-
 }
