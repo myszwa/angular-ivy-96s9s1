93be399f017cdf80a595971f5e166e4a7536dec6
myszwa <patryk.janikowski@gmail.com>
2022-08-17 15:47:36 +0200

    2 files modified, 2 added, 1 deleted

--- src/app/ngb-dropdown/ngb-dropdown.component.ts
+++ src/app/ngb-dropdown/ngb-dropdown.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-ngb-dropdown',
   templateUrl: './ngb-dropdown.component.html',
-  styleUrls: ['./ngb-dropdown.component.css']
+  styleUrls: ['./ngb-dropdown.component.scss'],
 })
 export class NgbDropdownComponent implements OnInit {
+  expandedList: boolean = false;
+  @Input() extendedList: any[];
+  @Input() choosenItem: string;
+  @Output() choosenItemChange = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  expandList() {
+    this.expandedList = !this.expandedList;
   }
 
+  selectItem(item: any) {
+    this.choosenItem = item;
+    this.choosenItemChange.emit(item);
+  }
 }
